b0e3dcb8a169584e09a50262727bbe71c7c3b561
cheraten <cheraten@yandex.ru>
2020-11-10 13:51:57 +0300

    [fix]: fix client-server

--- client_server/client.py
+++ client_server/client.py
@@ -5,20 +5,12 @@ from opcua import Client
 if __name__ == "__main__":
 
     client = Client("opc.tcp://localhost:4840/freeopcua/server/")
-    try:
-        client.connect()
-        root = client.get_root_node()
-        print("Objects node is: ", root)
-        print("Children of root are: ", root.get_children())
-        myvar = root.get_child(["0:Objects", "2:Object", "2:Variable"])
-        obj = root.get_child(["0:Objects", "2:Object"])
-        print("myvar is: ", myvar)
-        print("myobj is: ", obj)
+    client.connect()
+    var = client.get_node("ns=2;i=2")
 
-        var = client.get_node("ns=2;i=2")
-        while True:
-                value = var.get_value()
-                print("{0:.1f} units".format(value))
-                time.sleep(1)
-    finally:
-        client.disconnect()
+    while True:
+        value = var.get_value()
+        print("{0:.1f} units".format(value))
+        time.sleep(1)
+
+    client.disconnect()
--- client_server/server.py
+++ client_server/server.py
@@ -5,20 +5,19 @@ from opcua import ua, Server
 if __name__ == "__main__":
     server = Server()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
+
     ns = server.register_namespace("Concepts")
     objects = server.get_objects_node()
-
     myobj = objects.add_object(ns, "Object")
     myvar = myobj.add_variable(ns, "Variable", 0.0)
 
     server.start()
     
-    try:
-        count = 0
-        while True:
-            time.sleep(1)
-            count += 0.1
-            print("{:8.1f} units".format(count))
-            myvar.set_value(count)
-    finally:
-        server.stop()
+    count = 0
+    while True:
+        time.sleep(1)
+        count += 0.1
+        print("{:8.1f} units".format(count))
+        myvar.set_value(count)
+
+    server.stop()
